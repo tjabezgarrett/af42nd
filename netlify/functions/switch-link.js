719992df4deec82ac7124fdf9bc377de87dbac02
Tanner Garrett <tanner.garrett@tannerg9n0q547y.lan>
2024-07-03 21:50:16 -0600

    adjusted domain in CORS header

--- netlify/functions/switch-link.js
+++ netlify/functions/switch-link.js
@@ -12,7 +12,7 @@ exports.handler = async function(event, context) {
     return {
         statusCode: 200,
         headers: {
-            'Access-Control-Allow-Origin': 'https://tjabezgarrett.github.io/af42nd/',  // Allow only your GitHub Pages site
+            'Access-Control-Allow-Origin': 'https://tjabezgarrett.github.io',  // Allow only your GitHub Pages site
             'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
         },
         body: JSON.stringify({ link: link })
