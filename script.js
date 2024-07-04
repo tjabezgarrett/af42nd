a1d24c9d61f5b8a07f44af15e9b4abe31cd257d2
Tanner Garrett <tanner.garrett@tannerg9n0q547y.lan>
2024-07-03 21:36:35 -0600

    small updates for debugging

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 async function fetchLink() {
     try {
         const response = await fetch('https://silver-crumble-9cf8e6.netlify.app/.netlify/functions/switch-link');
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
         const data = await response.json();
         const dynamicLink = document.getElementById('dynamic-link');
         dynamicLink.href = data.link;
