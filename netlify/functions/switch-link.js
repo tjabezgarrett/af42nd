23ba38cc89ece0a91e40cbdef95c73e7fcaa545c
Tanner Garrett <tanner.garrett@tannerg9n0q547y.lan>
2024-07-03 21:47:43 -0600

    Added headers to allow CORS for only my domain

--- netlify/functions/switch-link.js
+++ netlify/functions/switch-link.js
@@ -11,6 +11,10 @@ exports.handler = async function(event, context) {
 
     return {
         statusCode: 200,
+        headers: {
+            'Access-Control-Allow-Origin': 'https://tjabezgarrett.github.io/af42nd/',  // Allow only your GitHub Pages site
+            'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+        },
         body: JSON.stringify({ link: link })
     };
 };
