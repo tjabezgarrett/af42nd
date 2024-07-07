26c5f4e8c888a779ce456cd45108e572a0bf2cba
Tanner Garrett <tanner.garrett@tannerg9n0q547y.lan>
2024-07-07 13:03:53 -0600

    working fix to cover raw link, additional styling

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+document.addEventListener('DOMContentLoaded', fetchLink);
+
 async function fetchLink() {
     try {
         const response = await fetch('https://silver-crumble-9cf8e6.netlify.app/.netlify/functions/switch-link');
@@ -7,10 +9,8 @@ async function fetchLink() {
         const data = await response.json();
         const dynamicLink = document.getElementById('dynamic-link');
         dynamicLink.href = data.link;
-        dynamicLink.textContent = "Endowed Member Recommend Renewal"; // OLD: data.link;
+        dynamicLink.textContent = "Endowed Member Recommend Renewal";
     } catch (error) {
         console.error('Error fetching the link:', error);
     }
 }
-
-document.addEventListener('DOMContentLoaded', fetchLink);
