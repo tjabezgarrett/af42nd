c2c6631661d0a91ac538671d867f705cd563f27f
Tanner Garrett <tanner.garrett@Tanner-Garrett-GX9N0Q547Y.local>
2024-08-11 10:10:57 -0600

    added no caching to js switch link

--- netlify/functions/switch-link.js
+++ netlify/functions/switch-link.js
@@ -1,4 +1,4 @@
-let currentIndex = 0;  // This should be stored in a persistent storage in a real application
+let currentIndex = 0;  // This should be stored in persistent storage in a real application
 
 const links = [
     'https://calendly.com/af42assistant1/tr',
@@ -9,12 +9,18 @@ exports.handler = async function(event, context) {
     currentIndex = (currentIndex + 1) % links.length;
     const link = links[currentIndex];
 
+    // Create a unique URL to prevent caching
+    const uniqueLink = `${link}?timestamp=${new Date().getTime()}`;
+
     return {
         statusCode: 200,
         headers: {
             'Access-Control-Allow-Origin': 'https://tjabezgarrett.github.io',  // Allow only your GitHub Pages site
-            'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+            'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+            'Cache-Control': 'no-cache, no-store, must-revalidate',  // No caching
+            'Pragma': 'no-cache',  // Older HTTP 1.0 backward compatibility
+            'Expires': '0'  // Expire immediately
         },
-        body: JSON.stringify({ link: link })
+        body: JSON.stringify({ link: uniqueLink })
     };
 };
